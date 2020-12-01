cca87aa6e1ed385bb12c7294af4a5015eceea861
koftov <ahronlu@gmail.com>
2020-12-01 13:33:17 +0200

    async custom middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,10 +1,20 @@
-export default ({ dispatch }) => (next) => (action) => {
+const async = ({ dispatch }) => (next) => (action) => {
   // Check to see if the action
   // has a promise on its "payload" property
   // If it does, then wait for it to resolve
   // if it doesn't, then send the action on to the
   // next middleware
   if (!action.payload || !action.payload.then) {
-    return next();
+    return next(action);
   }
+
+  // wait for the promise to resolve
+  // and then create a new action
+  // with that data and dispatch it
+  action.payload.then((response) => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
+
+export default async;
