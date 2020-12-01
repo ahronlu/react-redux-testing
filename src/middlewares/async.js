4890f7061fc6dbb74d42b9409af2ad6e92f77eed
koftov <ahronlu@gmail.com>
2020-12-01 13:22:33 +0200

    handling case when there isnt a promise

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -4,4 +4,7 @@ export default ({ dispatch }) => (next) => (action) => {
   // If it does, then wait for it to resolve
   // if it doesn't, then send the action on to the
   // next middleware
+  if (!action.payload || !action.payload.then) {
+    return next();
+  }
 };
